b2cd7374dc165e3c71d2628a5ccbf262f200c1d5
ram071985 <reidmuchow@gmail.com>
2021-01-28 16:04:54 -0600

    finished setting up modules and componenets.

--- src/app/components/todo-list/todo-list.module.ts
+++ src/app/components/todo-list/todo-list.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TodoListRoutingModule } from './todo-list-routing.module';
 import { TodoListComponent } from './todo-list.component';
 
